b2aba3e60bd4ef2884957747a1dfa53211d6dd8f
Jakob <jakobrosen0@gmail.com>
2026-04-02 16:24:43 +0200

    finished early version of navbar

--- Javascript2/src/Navbar.jsx
+++ Javascript2/src/Navbar.jsx
@@ -1,7 +1,15 @@
+import { Search } from "lucide-react";
+
 export default function Navbar() {
   return (
     <nav>
-      <NavHeader />
+      <div className="nav-content">
+        <NavHeader />
+        <div className="nav-controls">
+          <NavSearchBar />
+          <NavRegionDropdown />
+        </div>
+      </div>
     </nav>
   );
 }
@@ -12,5 +20,34 @@ function NavHeader() {
       <h1>Country Explorer</h1>
       <p>See information from countries around the world</p>
     </div>
+  );
+}
+
+function NavSearchBar() {
+  return (
+    <div className="nav-search-wrapper">
+      <Search size="15" />
+      <input
+        type="search"
+        id="nav-search-bar"
+        placeholder="Search countries..."
+      />
+    </div>
+  );
+}
+
+function NavRegionDropdown() {
+  return (
+    <>
+      <select>
+        <option value="All Regions">All Regions</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </>
   );
 }
