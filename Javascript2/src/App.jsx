9519ca726d74c9ad98cb014de37c72ea63fe99d3
Jonathan Henze <jonte055@hotmail.com>
2026-04-06 10:56:58 +0200

    First Commit Weather-widget

--- Javascript2/src/App.jsx
+++ Javascript2/src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./Navbar";
 import "./Navbar.css";
+import WeatherWidget from "./WeatherWidget";
+import "./WeatherWidget.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <WeatherWidget />
     </>
   );
 }
