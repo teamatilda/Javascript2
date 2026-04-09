f3b2bf7fb52bac9b7ed79319579063c219292958
Tonkis-code <toni.wecksell@gmail.com>
2026-04-09 11:11:00 +0200

    created coordinates for the capital city

--- Javascript2/src/Validators/weatherValidation.js
+++ Javascript2/src/Validators/weatherValidation.js
@@ -3,5 +3,12 @@ export function validateWeatherdata(data) {
         throw new Error("Invalid weather data");
     }
 
-    
+    const current = data.current
+
+    return {
+        temperature: current.temperature_2m ?? null,
+        windSpeed: current.wind_speed_10m ?? null,
+        weatherCode: current.weather_code ?? null,
+        time: current.time ?? "Unknown"
+    }
 }
