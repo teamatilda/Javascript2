a6fd110e6a10df4712213272a230a0e0418e0032
Tea Westling <teawest99@gmail.com>
2026-04-14 14:56:49 +0200

    implemented API and added flag in header

--- Javascript2/src/components/CountryInfo.jsx
+++ Javascript2/src/components/CountryInfo.jsx
@@ -1,34 +1,28 @@
 import "../styles/CountryInfo.css";
 import "../styles/index.css";
 import WeatherWidget from "./WeatherWidget";
+import "flag-icons/css/flag-icons.min.css";
 
 export default function CountryInfo({ country }) {
 
-const currency = country.identity?.currency?.name || "N/A";
-
   return (
     <section className="country-info-card">
-        {/* Header med namn på landet och flagga */}
-      <div className="country--info-header">
+      <div className="country-info-header">
+        <span className={`fi fi-${country.code}`} />
         <h1 className="country-info-title">{country.name}</h1>
-        <img src={country.flag} alt={country.name} />
-        {/* Kan behöva ändras beroende på hur vi tar in flaggan */}
       </div>
 
-    {/* Sektion med info om landet */}
       <div className="country-info-section">
         <InfoSection label="Capital" value={country.capital} />
-        <InfoSection label="Languages" value={country.languages} />
-        <InfoSection label="Currency" value={currency} />
+        <InfoSection label="Currency" value={country.currency} />
         <InfoSection label="Population" value={country.population} />
+        <InfoSection label="Languages" value={country.languages} />
       </div>
 
-    {/* Väder widget */}
-      <div className="-country-info-weather">
-        <WeatherWidget />
+      <div className="country-info-weather">
+        <WeatherWidget capital={country.capital} />
       </div>
 
-    {/* Info-box med historia och annan info */}
       <div className="country-info-box">
         <h2>History</h2>
         <p>Text here...{country.history}</p>
