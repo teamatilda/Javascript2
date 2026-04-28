b542f25d84293c66d004f92741472ffab9e4db76
Merilii Kalvik <merilii@hotmail.com>
2026-04-28 22:01:57 +0300

    connect country data to designed cards, improve layout and styling

--- Javascript2/src/App.jsx
+++ Javascript2/src/App.jsx
@@ -4,11 +4,7 @@ import "./styles/Navbar.css";
 import "./style.css";
 import CountryInfoDetails from "./pages/CountryInfoPage.jsx";
 import CountryDetails from "./pages/CountryDetails.jsx";
-<<<<<<< 40-add-facts
 import CountryInfo from "./components/CountryInfo.jsx";
-=======
-import Home from "./pages/Home.jsx";
->>>>>>> main
 import { getWeather } from "./api/weatherApi";
 import FlagQuizPage from "./pages/FlagQuizPage.jsx";
 import { useCountriesStore } from "./store/countriesStore.js";
@@ -21,12 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<CountryDetails />} />
         <Route path="/Country/:info" element={<CountryInfoDetails />} />
-<<<<<<< 40-add-facts
         <Route path="/Quiz" element={<FlagQuizPage />} />
-=======
-        <Route path="/FlagQuiz" element={<FlagQuizPage />} />
-        <Route path="/Country/" element={<CountryDetails />} />
->>>>>>> main
       </Routes>
       <Footer />
     </>
